c5bfc567c66722fcd77e0d8ba1b54cf469f143f5
C四 <flyships@qq.com>
2022-05-28 08:18:37 +0800

    减去一些繁琐过程

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -7,13 +7,11 @@ from urllib.request import urlopen
 
 string = "\/:*?\">|"
 path = "path"
-
+mode = 0
 
 def Start():
     global path
-    print("C Beadd\n")
-    print("歌曲自动下载至当前目录MusicB中\n歌词自动下载至当前目录LyricB中\n")
-    mode = input("下载网易云单曲\t1\n下载网易云歌单\t2\n输入数字选择:\t")
+    global mode
     if mode == "1":
         os.system("cls")
         id = input("请输入单曲ID:")
@@ -32,14 +30,12 @@ def Music():
         source = response.read()
     data = json.loads(source)
     if 'error' in data:
-        print("ID有错误!请检查")
-        os.system("pause")
-        os._exit(1)
+        return 0;
 
     # start
     os.system("mkdir MusicB")
     for data in data:
-        os.system("cls")
+        # os.system("cls")
         print( str(counter) + ' ' + data['name'])
         name = data['name']
         for i in string:
@@ -60,12 +56,11 @@ def Lyric():
     with urlopen(path) as response:
         source = response.read()
     data = json.loads(source)
-
     # start
     os.system("mkdir LyricB")
     for data in data:
-        os.system("cls")
-        print( str(counter) + ' ' + data['name'])
+        # os.system("cls")
+        print( str(counter) + ' ' + data['name'] + '(歌词)')
         name = data['name']
         for i in string:
             if i in name:
@@ -77,13 +72,19 @@ def Lyric():
             code.write(req.content)
         counter += 1
 
-
+print("C Beadd")
+print("歌曲自动下载至当前目录MusicB中\n歌词自动下载至当前目录LyricB中\n")
 while True:
+    if mode == 0:
+        mode = input("下载网易云单曲\t1\n下载网易云歌单\t2\n输入数字选择:\t")
     Start()
     print("开始下载歌曲")
-    Music()
-    print("歌曲下载完成!是否下载歌词?\n取消请直接关闭窗口")
-    os.system("pause")
-    Lyric()
-    print("下载完成!感谢使用!\n继续再次选择下载\n")
-    os.system("pause")
+    if Music() == 0:
+        print("ID有错误!请检查")
+        os.system("pause")
+    # print("歌曲下载完成!是否下载歌词?\n取消请直接关闭窗口")
+    # os.system("pause")
+    else:
+        Lyric()
+        print("下载完成!感谢使用!\n请直接关闭窗口或继续\n")
+        os.system("pause")
