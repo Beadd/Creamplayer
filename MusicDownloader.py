1bcf330830dc7b3826371c7562b759ce093b549d
C四 <flyships@qq.com>
2022-05-30 09:44:06 +0800

    更换了获取方式增加了Header与Proxies

---
+++ MusicDownloader.py
@@ -0,0 +1,125 @@
+import json
+import re
+from traceback import print_tb
+import requests
+import os
+
+from urllib.request import urlopen
+
+string = "\/:*?\">|"
+path = "path"
+mode = 0
+header = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
+}
+proxies = {"http": None, "https": None}
+
+def Start():
+    global path
+    global mode
+    if mode == "1":
+        os.system("cls")
+        id = input("请输入网易云单曲ID:")
+        path = "http://api.injahow.cn/meting/?type=song&id=" + str(id)
+        return 1
+    if mode == "2":
+        os.system("cls")
+        id = input("请输入网易云歌单ID:")
+        path = "http://api.injahow.cn/meting/?type=playlist&id=" + str(id)
+        return 1
+    if mode == "3":
+        os.system("cls")
+        id = input("请输入QQ音乐单曲ID:")
+        path = "http://api.injahow.cn/meting/?server=tencent&type=song&id=" + str(id)
+        return 1
+    if mode == "4":
+        os.system("cls")
+        id = input("请输入QQ音乐歌单ID:")
+        path = "http://api.injahow.cn/meting/?server=tencent&type=playlist&id=" + str(id)
+        return 1
+    
+    else:
+        return 0
+
+    
+def Music():
+    counter = 1
+    # file = open(path, encoding="utf-8")
+    # data = json.load(file)
+    #  with urlopen(path) as response:
+    #    source = response.read()
+    """source == response"""
+    response = requests.get(path, headers=header, proxies=proxies)
+    data = json.loads(response.text)
+    if 'error' in data:
+        return 0;
+
+    # start
+    os.system("mkdir MusicB")
+    for data in data:
+        # os.system("cls")
+        print( str(counter) + ' ' + data['name'])
+        name = data['name']
+        for i in string:
+            if i in name:
+                name = "NameFalseNo." + str(counter)
+        name_url = "MusicB/" + name + ".mp3"
+        url = data['url']
+        req = requests.get(url)
+        with open(name_url, "wb") as code:
+            code.write(req.content)
+        counter += 1
+
+
+def Lyric():
+    counter = 1
+    # file = open(path, encoding="utf-8")
+    # data = json.load(file)
+    # with urlopen(path) as response:
+    #    source = response.read()
+    """source == response"""
+    response = requests.get(path, headers=header, proxies=proxies)
+    data = json.loads(response.text)
+    # start
+    os.system("mkdir LyricB")
+    for data in data:
+        # os.system("cls")
+        print( str(counter) + ' ' + data['name'] + '(歌词)')
+        name = data['name']
+        for i in string:
+            if i in name:
+                name = "NameFalseNo." + str(counter)
+        name_url = "LyricB/" + name + ".lrc"
+        url = data['lrc']
+        req = requests.get(url)
+        with open(name_url, "wb") as code:
+            code.write(req.content)
+        counter += 1
+
+os.system("cls")
+print("C Beadd")
+print("歌曲自动下载至当前目录MusicB中\n歌词自动下载至当前目录LyricB中\n")
+while True:
+    if mode == 0:
+        print("下载网易云单曲\t1")
+        print("下载网易云歌单\t2")
+        print("下载QQ音乐单曲\t3")
+        print("下载QQ音乐歌单\t4")
+        
+        mode = input("输入数字选择:\t")
+    if Start() == 0:
+        mode = 0
+        os.system("cls")
+        print("请输入正确数字!")
+    else:
+        print("开始下载歌曲")
+        if Music() == 0:
+            print("ID有错误!请检查")
+            os.system("pause")
+        # print("歌曲下载完成!是否下载歌词?\n取消请直接关闭窗口")
+        # os.system("pause")
+        else:
+            Lyric()
+            print("下载完成!感谢使用!\n请直接关闭窗口或继续\n")
+            os.system("pause")
