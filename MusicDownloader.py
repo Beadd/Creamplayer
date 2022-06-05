2e593be33a75dc5027e733e3c55c30af916d4c45
C四 <flyships@qq.com>
2022-06-05 14:10:25 +0800

    Cookie

--- MusicDownloader.py
+++ MusicDownloader.py
@@ -18,6 +18,7 @@ proxies = {"http": None, "https": None}
 header = {
     'User-Agent':
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
+    #,'cookie':
 }
 header163 = {
     'User-Agent':
